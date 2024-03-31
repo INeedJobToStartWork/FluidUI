1c9dc80e1e1853491d0b1944a537227f9eee4a53
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2024-03-31 17:37:15 +0200

    ⚙️  Chore(docs): Initialize Docs with Nextra

---
+++ apps/docs/next.config.js
@@ -0,0 +1,11 @@
+import NextraConfig from "nextra";
+
+const withNextra = NextraConfig({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+export default withNextra();
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
